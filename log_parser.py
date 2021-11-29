489bf79db7fc8ad003b127c1b683eb5dc45be1bd
Tobias Schneeberger <tobiassamuel.schneeberger@students.bfh.ch>
2021-11-29 15:36:54 +0100

    minor changes

--- log_parser.py
+++ log_parser.py
@@ -1,16 +1,18 @@
 import os
 import re
-import pickle # for storing some data
+import pickle# for storing some data
 
 # Regex used to match relevant loglines (in this case, a specific IP address)
 FILTER_MODSECURITY = re.compile(r".*ModSecurity.*$")
-LINE_PATTERN = re.compile(r"(\S+) (\S+) \[(\S+)] .* ModSecurity: (.+).*$")
-LINE_PATTERN_temp = '(\S+) (\S+) [(\S+)] (\S+) ModSecurity: (\S+) [file "(\S+).conf(\S+) [id "(\S+)"(\S+) [msg "(\S+)"] (\S+)[ver "(\S+)"](\S+)] [tag (\S+)] [host<_>, client: (\S+), server: (\S+), request: "(\S+) (\S+) '
+LINE_PATTERN_NGINX_ERROR = re.compile(r"(\S+) (\S+) \[(\S+)] .* ModSecurity: (.+).*$")
+LINE_PATTERN_temp = '(\S+) (\S+) [(\S+)] (\S+) ModSecurity: (\S+) [file "(\S+).conf(\S+) [id "(\S+)"(\S+) [msg "(\S+)"]' \
+                    ' (\S+)[ver "(\S+)"](\S+)] [tag (\S+)] [host<_>, client: (\S+), server: (\S+), request: "(\S+) (\S+) '
+
 # Output file, where the matched loglines will be copied to
 output_filename = os.path.normpath("output/parsed_lines.log")
 # Overwrites the file, ensure we're starting out with a blank file
-with open(output_filename, "w") as out_file:
-    out_file.write("")
+# with open(output_filename, "w") as out_file:
+    # out_file.write("")
 
 # Open output file in 'append' mode
 with open(output_filename, "a") as out_file:
@@ -20,7 +22,7 @@ with open(output_filename, "a") as out_file:
         for line in in_file:
             # If log line matches our regex, print to console, and output file
             if FILTER_MODSECURITY.search(line):
-                match = LINE_PATTERN.search(line)
+                match = LINE_PATTERN_NGINX_ERROR.search(line)
                 print(line)
 
                 print(match.group(1))
