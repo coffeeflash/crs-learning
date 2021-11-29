fd936ba89965d2a33df1c19f7483711242f00dd4
Schneeberger Tobias <tobiassamuel.schneeberger@students.bfh.ch>
2021-11-29 20:15:18 +0100

    regex nginx extended

--- log_parser.py
+++ log_parser.py
@@ -1,10 +1,11 @@
 import os
 import re
-import pickle# for storing some data
+import pickle  # for storing some data
 
 # Regex used to match relevant loglines (in this case, a specific IP address)
 FILTER_MODSECURITY = re.compile(r".*ModSecurity.*$")
-LINE_PATTERN_NGINX_ERROR = re.compile(r"(\S+) (\S+) \[(\S+)] .* ModSecurity: (.+).*$")
+LINE_PATTERN_NGINX_ERROR = re.compile(
+    r"(\S+) (\S+) \[(\S+)] .* ModSecurity: (.+) \[file \"(\S+).conf.* \[id \"(\S+)\"].* \[msg \"(.+)\"].*, server: (\S+), request: \"(\S+) (\S+) .*$")
 LINE_PATTERN_temp = '(\S+) (\S+) [(\S+)] (\S+) ModSecurity: (\S+) [file "(\S+).conf(\S+) [id "(\S+)"(\S+) [msg "(\S+)"]' \
                     ' (\S+)[ver "(\S+)"](\S+)] [tag (\S+)] [host<_>, client: (\S+), server: (\S+), request: "(\S+) (\S+) '
 
@@ -12,7 +13,7 @@ LINE_PATTERN_temp = '(\S+) (\S+) [(\S+)] (\S+) ModSecurity: (\S+) [file "(\S+).c
 output_filename = os.path.normpath("output/parsed_lines.log")
 # Overwrites the file, ensure we're starting out with a blank file
 # with open(output_filename, "w") as out_file:
-    # out_file.write("")
+# out_file.write("")
 
 # Open output file in 'append' mode
 with open(output_filename, "a") as out_file:
@@ -29,5 +30,11 @@ with open(output_filename, "a") as out_file:
                 print(match.group(2))
                 print(match.group(3))
                 print(match.group(4))
+                print(match.group(5))
+                print(match.group(6))
+                print(match.group(7))
+                print(match.group(8))
+                print(match.group(9))
+                print(match.group(10))
 
                 out_file.write(line)
